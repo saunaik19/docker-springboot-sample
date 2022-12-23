ac0afe768e5af750ed93763cffe14e88b9b35cd7
Saurabh Govind Naik <saunaik19@gmail.com>
2022-12-23 18:13:56 +0530

    Initial commit

---
+++ src/test/java/com/example/dockerspringbootsample/DockerSpringbootSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringbootsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringbootSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
